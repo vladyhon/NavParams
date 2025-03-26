5e149a282a8814c1e6b3307869ddc50266ca4d07
Vladyslav H. <vladislav.hontar@gmail.com>
2025-03-26 15:32:23 +0100

    Add creation function for ConsumableNavParameter (#4)

--- lib/src/main/kotlin/io/github/vladyhon/navparams/ConsumableNavParameter.kt
+++ lib/src/main/kotlin/io/github/vladyhon/navparams/ConsumableNavParameter.kt
@@ -1,5 +1,12 @@
 package io.github.vladyhon.navparams
 
+import io.github.vladyhon.navparams.internal.consumable.ConsumableNavParameterImpl
+
+public fun <T : Any> ConsumableNavParameter(
+    value: T,
+    consumed: suspend () -> Unit,
+): ConsumableNavParameter<T> = ConsumableNavParameterImpl(value, consumed)
+
 public interface ConsumableNavParameter<T : Any> {
     public val value: T
     public suspend fun consume()
@@ -7,3 +14,7 @@ public interface ConsumableNavParameter<T : Any> {
 
 public suspend fun <T : Any> ConsumableNavParameter<T>.getAndConsume(): T =
     value.apply { consume() }
+
+public fun <T : Any> T.asConsumableNavParameter(
+    consumed: suspend () -> Unit,
+): ConsumableNavParameter<T> = ConsumableNavParameterImpl(this, consumed)
